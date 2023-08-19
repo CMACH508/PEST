681d79e61c979db347a4f9c555f1ec54a39b3a57
CMACH508 <52200550+CMACH508@users.noreply.github.com>
2023-08-19 10:38:51 +0800

    Add files via upload

---
+++ src/transformer.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+from einops import rearrange, repeat
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x, src_key_padding_mask=None):
+        # x: batch_size n_patch dim
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        if src_key_padding_mask is not None:
+            src_key_padding_mask = src_key_padding_mask.unsqueeze(1).unsqueeze(2)
+            dots = dots.masked_fill(src_key_padding_mask, float('-inf'))
+
+
+        attn = self.attend(dots)
+        # print(attn)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+
+
+class Attention_structure(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.cnn = nn.Sequential(nn.Conv2d(3, heads, kernel_size=1), nn.ReLU(), nn.Conv2d(heads, heads, kernel_size=1))
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x, dist, src_key_padding_mask=None):
+
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        dist1 = dist / 3.8; dist2 = dist1 * dist1; dist3 = dist2 * dist1
+        dist1 = 1/(1+dist1); dist2 = 1/(1+dist2); dist3 = 1/(1+dist3)
+        dist = torch.cat([dist1.unsqueeze(1), dist2.unsqueeze(1), dist3.unsqueeze(1)], dim=1)
+        dist = self.cnn(dist)
+        dots = dots + dist
+
+        if src_key_padding_mask is not None:
+            src_key_padding_mask = src_key_padding_mask.unsqueeze(1).unsqueeze(2)
+            dots = dots.masked_fill(src_key_padding_mask, float('-inf'))
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x, src_key_padding_mask=None):
+        # x: batch_size n_patch dim
+        for attn, ff in self.layers:
+            x = attn(x, src_key_padding_mask=src_key_padding_mask) + x
+            x = ff(x) + x
+        return x
+
+
+class Transformer_structure(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention_structure(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x, dist, src_key_padding_mask=None):
+        for attn, ff in self.layers:
+            x = attn(x=x, dist=dist, src_key_padding_mask=src_key_padding_mask) + x
+            x = ff(x) + x
+        return x
+
+
+if __name__ == '__main__':
+    dim = 1024
+    depth = 3
+    heads = dim//64
+    dim_head = 64
+    mlp_dim = dim*4
+    dropout = 0.1
+
+    transformer = Transformer_structure(dim, depth, heads, dim_head, mlp_dim, dropout).cuda()
+    x = torch.rand([1, 4, 1024]).cuda()
+    m = torch.ones([1, 4], dtype=bool).cuda()
+    m[:, :2] = False
+    # print(m)
+    d = torch.rand([1, 4, 4]).cuda()
+    out = transformer(x, d, m)
+    print(out.shape)
+    
